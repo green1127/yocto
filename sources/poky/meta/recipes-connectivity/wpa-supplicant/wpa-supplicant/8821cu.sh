94858c5546bf4348ec75d1c85c23dad62571e9a2
张思远 <siyuan.zhang@enjoymove.cn>
2023-01-09 21:40:11 +0800

    modules dir use uname -r

--- sources/poky/meta/recipes-connectivity/wpa-supplicant/wpa-supplicant/8821cu.sh
+++ sources/poky/meta/recipes-connectivity/wpa-supplicant/wpa-supplicant/8821cu.sh
@@ -24,7 +24,7 @@ echo 1 > /sys/class/gpio/gpio90/value
 echo "8821cu done"
 
 echo "load sja1105.ko"
-insmod /lib/modules/5.10.41-rt42+gb5dbd57c1cb2/kernel/drivers/spi/sja1105pqrs.ko
+insmod /lib/modules/$(uname -r)/kernel/drivers/spi/sja1105pqrs.ko
 
 COUNT_MAX=60
 
